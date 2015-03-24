1df4581718bfcd8b70ecb0c827a29f4756e13e87
AlexKnauth <alexander@knauth.org>
2015-03-23 21:04:44 -0400

    add newlines between deps

--- info.rkt
+++ info.rkt
@@ -2,7 +2,14 @@
 
 (define collection 'multi)
 
-(define deps '("base" "rackjure"))
+(define deps '("base"
+               "rackjure"
+               ))
 
-(define build-deps '("rackunit-lib" "htdp-lib" "racket-doc" "scribble-lib" "sandbox-lib"))
+(define build-deps '("rackunit-lib"
+                     "htdp-lib"
+                     "racket-doc"
+                     "scribble-lib"
+                     "sandbox-lib"
+                     ))
 
